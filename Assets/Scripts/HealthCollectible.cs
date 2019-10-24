30b84ebaa8c7988351dac92ad25bab9bda65aeeb
Geoffrey Jount <geoffrey.jount@epita.fr>
2019-10-24 08:30:17 +0200

    Make collectible conditional upon HP.

--- Assets/Scripts/HealthCollectible.cs
+++ Assets/Scripts/HealthCollectible.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class HealthCollectible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
+        RubyController controller = other.GetComponent<RubyController>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (controller != null && controller.health < controller.maxHealth)
+        {
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -5,6 +5,7 @@ public class RubyController : MonoBehaviour
 {
     public int maxHealth = 5;
     int currentHealth;
+    public int health { get { return currentHealth; }}
 
     public float speed = 3.0f;
 
@@ -14,7 +15,7 @@ public class RubyController : MonoBehaviour
     void Start()
     {
         rigidbody2d = GetComponent<Rigidbody2D>();
-	currentHealth = maxHealth;
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
@@ -31,9 +32,9 @@ public class RubyController : MonoBehaviour
         rigidbody2d.MovePosition(position);
     }
 
-    void ChangeHealth(int amount)
+    public void ChangeHealth(int amount)
     {
-	currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
-	Debug.Log(currentHealth + "/" + maxHealth);
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
+        Debug.Log(currentHealth + "/" + maxHealth);
     }
 }
