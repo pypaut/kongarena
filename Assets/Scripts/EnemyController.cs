cc37719acf4faeca3c7d97f8b89a93401e67b785
Geoffrey Jount <geoffrey.jount@epita.fr>
2019-10-24 09:30:10 +0200

    Add moving enemy.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed = 5.0f;
+    public bool vertical = true;
+    public float changeTime = 3.0f;
+
+    Rigidbody2D rigidbody;
+    float timer;
+    int direction = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        timer = changeTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+
+        Vector2 position = rigidbody.position;
+        if (vertical)
+        {
+            position.y = position.y + Time.deltaTime * speed * direction;
+        }
+        else
+        {
+            position.x = position.x + Time.deltaTime * speed * direction;
+        }
+
+        rigidbody.MovePosition(position);
+    }
+}
