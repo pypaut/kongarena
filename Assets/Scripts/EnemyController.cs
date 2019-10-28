8d8145b75af77fd3d5673cd354c4f11f3a8ef4c2
Geoffrey Jount <geoffrey.jount@epita.fr>
2019-10-28 08:14:49 +0100

    Robot animator controller.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -11,12 +11,14 @@ public class EnemyController : MonoBehaviour
     Rigidbody2D rigidbody;
     float timer;
     int direction = 1;
+    Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         rigidbody = GetComponent<Rigidbody2D>();
         timer = changeTime;
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -33,10 +35,14 @@ public class EnemyController : MonoBehaviour
         Vector2 position = rigidbody.position;
         if (vertical)
         {
+            animator.SetFloat("Move X", 0);
+            animator.SetFloat("Move Y", direction);
             position.y = position.y + Time.deltaTime * speed * direction;
         }
         else
         {
+            animator.SetFloat("Move X", direction);
+            animator.SetFloat("Move Y", 0);
             position.x = position.x + Time.deltaTime * speed * direction;
         }
 
